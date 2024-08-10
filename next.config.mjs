8af4906a7c7b308856fde08fe56e59bb5c4acd74
illialaz <illialaziuk2@gmail.com>
2024-08-10 20:59:19 +0300

    Initialize app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: process.env.NODE_ENV === 'development' ? '.dev' : undefined,
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default nextConfig;
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  singleQuote: true,
+  semi: true,
+};
+
+export default config;
